8d92114ff03594b028df14b76f8722dd4128de93
ElironB <Elironb11@gmail.com>
2024-05-19 00:43:33 +0300

    Hopefuly last

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -3,11 +3,7 @@ import { NextResponse } from 'next/server';
 import { headers } from 'next/headers';
 
 import { stripe } from '../../../lib/stripe';
-import {
-    upsertProductRecord,
-    upsertPriceRecord,
-    manageSubscriptionStatusChange
-} from "../../../lib/supabaseAdmin";
+
 
 const relevantEvents = new Set([
     'product.created',
@@ -43,35 +39,9 @@ export async function POST(
         try {
             switch (event.type) {
                 case 'product.created':
-                case 'product.updated':
-                    await upsertProductRecord(event.data.object as Stripe.Product);
-                    break;
                 case 'price.created':
-                case 'price.updated':
-                    await upsertPriceRecord(event.data.object as Stripe.Price);
-                    break;
                 case 'customer.subscription.created':
                 case 'customer.subscription.updated':
-                case 'customer.subscription.deleted':
-                    const subscription = event.data.object as Stripe.Subscription;
-                    await manageSubscriptionStatusChange(
-                        subscription.id,
-                        subscription.customer as string,
-                        event.type === 'customer.subscription.created'
-                    );
-                    break;
-                case 'checkout.session.completed':
-                    const checkoutSession = event.data
-                        .object as Stripe.Checkout.Session;
-                    if (checkoutSession.mode === 'subscription') {
-                        const subscriptionId = checkoutSession.subscription;
-                        await manageSubscriptionStatusChange(
-                            subscriptionId as string,
-                            checkoutSession.customer as string,
-                            true
-                        );
-                    }
-                    break;
                 default:
                     throw new Error('Unhandled relevant event!');
             }
